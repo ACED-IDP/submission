afebefdb0b86baaeec676b74f3a3808870234216
Brian Walsh <brian@bwalsh.com>
2023-06-01 09:52:31 -0700

    Adds iceberg publish

--- aced_submission/meta.py
+++ aced_submission/meta.py
@@ -35,6 +35,27 @@ meta.add_command(dir_to_study)
 meta.add_command(simplifier)
 
 
+@meta.command(name='publish')
+@click.argument('dictionary_path', default='iceberg/schemas/gen3/aced.json')
+@click.option('--bucket', default="s3://aced-public", help="Bucket target", show_default=True)
+@click.option('--production', default=False, is_flag=True, show_default=True,
+              help="Write to aced.json, otherwise aced-test.json")
+def schema_publish(dictionary_path, bucket, production):
+    """Copy dictionary to s3 (note:aws cli dependency)"""
+
+    dictionary_path = pathlib.Path(dictionary_path)
+    assert dictionary_path.is_file(), f"{dictionary_path} should be a path"
+    click.echo(f"Writing schema into {bucket}")
+    import subprocess
+    if production:
+        cmd = f"aws s3 cp {dictionary_path} {bucket}".split(' ')
+    else:
+        cmd = f"aws s3 cp {dictionary_path} {bucket}/aced-test.json".split(' ')
+    s3_cp = subprocess.run(cmd)
+    assert s3_cp.returncode == 0, s3_cp
+    print("OK")
+
+
 @meta.command('validate')
 @click.option('--path', required=True, default=None, show_default=True,
               help='Path to FHIR files.')
