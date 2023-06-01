a36641ce6283544d76724d50c8e70b01df964b47
Brian Walsh <brian@bwalsh.com>
2023-06-01 09:52:01 -0700

    Adds fastjsonschema

--- aced_submission/gen_validator.py
+++ aced_submission/gen_validator.py
@@ -4,7 +4,6 @@ from typing import Iterator
 
 import fastjsonschema
 import inflection
-import jsonschema
 import orjson
 import requests
 from dictionaryutils import DataDictionary
@@ -54,7 +53,11 @@ def _validate(gen3_resource: dict, schemas: dict):
                 schema,
                 formats={
                     'time': r'^(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$',
-                    'date': r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])?$'
+                    'date': r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])?$',
+                    'binary': r'^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$',
+                    "date-time": r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$',
+                    "uri": r'\w+:(\/?\/?)[^\s]+',
+                    "uuid": r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$"
                 }
             )
             COMPILED_SCHEMAS[key] = compiled_schema
