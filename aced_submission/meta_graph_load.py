902266ca73215c63fcea30e8e98ad78d2a7f53ce
Brian Walsh <brian@bwalsh.com>
2023-07-19 23:05:43 -0700

    fix logging ResearchStudy->Project

--- aced_submission/meta_graph_load.py
+++ aced_submission/meta_graph_load.py
@@ -132,7 +132,7 @@ def load_edges(files, connection, dependency_order, mapping, project_node_id):
                         if d_['name'] in ['ResearchStudy', 'research_study']:
                             # link the ResearchStudy to the gen3 project
                             relations.append({"dst_id": project_node_id, "dst_name": "Project", "label": "project"})
-                            logger.info(f"adding project relation from project({project_node_id}) to research_study{d_['id']} {d_['code']}")
+                            logger.info(f"adding project relation from project({project_node_id}) to research_study{d_['id']}")
 
                         if len(relations) == 0:
                             continue
