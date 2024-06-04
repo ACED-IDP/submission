d11a89ccd3423777926f5dbfe14de054f2caed98
matthewpeterkort <matthewpeterkort@gmail.com>
2024-06-04 15:34:18 -0700

    construction code

--- aced_submission/fhir_store.py
+++ aced_submission/fhir_store.py
@@ -43,7 +43,7 @@ def fhir_put(project_id, path, elastic_url) -> list[str]:
     for file_path in pathlib.Path(path).glob('*.ndjson'):
 
         write_bulk_http(elastic=elastic, index=index, doc_type=doc_type, limit=limit,
-                        generator=resource_generator(project_id, file_path), schema=None)
+                        generator=resource_generator(project_id, file_path))
 
         logs.append(f"wrote {file_path} to elasticsearch/{index}")
 
