7d588984a894bb61fd2f00eee2bf29238d06bc0a
quinnwai <quinnwai.wong@gmail.com>
2024-09-03 12:31:55 -0700

    support capability for long for larger than 32 bit numbers

--- aced_submission/meta_flat_load.py
+++ aced_submission/meta_flat_load.py
@@ -71,7 +71,10 @@ def generate_elasticsearch_mapping(df: List[Dict]) -> Dict[str, Any]:
     """
 
     def is_integer_dtype(value: Any) -> bool:
-        return isinstance(value, int) and not isinstance(value, bool)
+        return isinstance(value, int) and value.bit_length() <= 32 and not isinstance(value, bool)
+    
+    def is_long_dtype(value: Any) -> bool:
+        return isinstance(row[column], int) and row[column].bit_length() > 32
 
     def is_float_dtype(value: Any) -> bool:
         return isinstance(value, float)
@@ -108,6 +111,8 @@ def generate_elasticsearch_mapping(df: List[Dict]) -> Dict[str, Any]:
         for column in row.keys():
             if is_integer_dtype(row[column]):
                 mapping["mappings"]["properties"][column] = {"type": "integer"}
+            elif is_long_dtype(row[column]):
+                mapping["mappings"]["properties"][column] = {"type": "long"}
             elif is_float_dtype(row[column]):
                 mapping["mappings"]["properties"][column] = {"type": "float"}
             elif is_bool_dtype(row[column]):
@@ -121,6 +126,7 @@ def generate_elasticsearch_mapping(df: List[Dict]) -> Dict[str, Any]:
                     mapping["mappings"]["properties"][column] = {"type": "keyword"}
             elif isinstance(row[column], str):
                 mapping["mappings"]["properties"][column] = {"type": "keyword"}
+    
     return mapping
 
 
