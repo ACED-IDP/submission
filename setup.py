f0a4128a49da4851e79dc09fa2514febac8f44b5
Matthew Peterkort <33436238+matthewpeterkort@users.noreply.github.com>
2025-08-19 15:04:23 -0700

    Fix/pg version (#36)
    * rename nginx path name to appropriate name
    
    * fix expected data field
    
    * fix grip load to work with latest grip
    
    * pass chunk size as arg
    
    * commit setup version
    
    * route through nginx instead of directly to grip
    
    * bump version
    
    * make pg version compat with python verison
    
    * add setup version
    
    * rename nginx path name to appropriate name
    
    * fix expected data field
    
    * route through nginx instead of directly to grip
    
    * bump version
    
    * bump pypi version

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ setup(
 
     # Versions should comply with PEP 440:
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.10rc12',  # Required
+    version='0.0.10rc14',  # Required
+
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
